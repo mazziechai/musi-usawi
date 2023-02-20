b4a0d9913031f4f8cd82bfa6fc8ce4c178f26ecb
Mazzie <mazziechai@gmail.com>
2023-02-19 16:18:49 -0800

    Template

---
+++ lwjgl3/src/main/kotlin/cafe/ferret/musi/lwjgl3/Lwjgl3Launcher.kt
@@ -0,0 +1,16 @@
+@file:JvmName("Lwjgl3Launcher")
+
+package cafe.ferret.musi.lwjgl3
+
+import cafe.ferret.musi.Game
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+
+/** Launches the desktop (LWJGL3) application. */
+fun main() {
+    Lwjgl3Application(Game(), Lwjgl3ApplicationConfiguration().apply {
+        setTitle("musi-toki")
+        setWindowedMode(640, 480)
+        setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
+    })
+}
